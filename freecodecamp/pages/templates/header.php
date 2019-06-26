4e89e090a1b8836ad0bc649bf9a030a63c6287a5
etukleris <49657933+etukleris@users.noreply.github.com>
2019-06-26 20:17:34 +0300

    header and footer split up
    header and footer split up and used as separate imports as site moved from using just html to php

---
+++ freecodecamp/pages/templates/header.php
@@ -0,0 +1,22 @@
+<header class="header header-footer">
+  <h1>Titlerooni, checking out the basics</h1>
+  <nav>
+    <div class="navbar">
+      <a href="index.php">Home</a>
+      <a href="photos.php">Photos</a>
+      <div class="dropdown">
+        <button class="dropbtn">CSS tests 
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="csstest1.php">Css1</a>
+          <a href="csstest2.php">Css2</a>
+          <a href="csstest3.php">Css3</a>
+          <a href="csstest4.php">Css4</a>
+          <a href="csstest5.php">Css5</a>
+          <a href="csstest6.php">Css6</a>
+        </div>
+      </div> 
+    </div>
+  </nav>
+</header>
