1a8a44c239793f26de8956b85d40c07bf46ac84c
etukleris <49657933+etukleris@users.noreply.github.com>
2019-07-08 13:53:51 +0300

    additional dummy page update

--- freecodecamp/pages/dummy.php
+++ freecodecamp/pages/dummy.php
@@ -14,7 +14,7 @@
 
   <main class="dummy-page">
     <h1> Dummy page to test git, branches and merging </h1>
-    <p>with some dummy text</p>
+    <p>with some extra dummy text</p>
     <p>some extra text for pull test</p>
   </main>
   
