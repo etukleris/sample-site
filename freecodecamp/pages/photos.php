4c2935deecf9e495ac6138689b5e0d520f43d47c
etukleris <49657933+etukleris@users.noreply.github.com>
2019-07-08 11:49:04 +0300

    Changed some css for photos page

--- freecodecamp/pages/photos.php
+++ freecodecamp/pages/photos.php
@@ -28,9 +28,9 @@
         echo '<div class="posts-container">';
         // output data of each row
           while($row = $result->fetch_assoc()) {
-            echo '<div class="post-images">';
-              echo '<img class="user-image" src="../uploaded-pictures/'.$row["imagename"].'" onerror="this.onerror=null; this.src=\'../uploaded-pictures/noimage2.png\'" alt="User\'s image">';
-              echo '<div class="hover-image-container">';
+            echo '<div class="post-images container">';
+              echo '<img class="user-image image" src="../uploaded-pictures/'.$row["imagename"].'" onerror="this.onerror=null; this.src=\'../uploaded-pictures/noimage2.png\'" alt="User\'s image">';
+              echo '<div class="hover-image-container overlay">';
                 echo '<p class="hover-image-text">' . $row["useradded"] . '\'s image.Cat type: ' . $row["cattype"].'<br>'; 
                 if ($row["catcharacteristics"] == "NULL"){
                   echo 'Cat characteristics: Regular </p>';
