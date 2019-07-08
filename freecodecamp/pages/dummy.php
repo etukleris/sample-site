397099ac32c8cf1c9177b2a6431c3d4570798d3e
etukleris <49657933+etukleris@users.noreply.github.com>
2019-07-08 13:50:35 +0300

    Minor update on dummy page
    learning git pull feature

--- freecodecamp/pages/dummy.php
+++ freecodecamp/pages/dummy.php
@@ -13,8 +13,9 @@
   <!-- header-->
 
   <main class="dummy-page">
-  <h1> Dummy page to test git, branches and merging </h1>
-  <p>with some dummy text</p>
+    <h1> Dummy page to test git, branches and merging </h1>
+    <p>with some dummy text</p>
+    <p>some extra text for pull test</p>
   </main>
   
   <!--import footer-->
